8e314fbb68cc85bc14ac919b194d6ca272bf3483
omer <omer@cywift.com>
2025-03-29 07:59:53 +0300

    Service

--- iac/main.tf
+++ iac/main.tf
@@ -41,3 +41,50 @@ output "example_password" {
   sensitive = true
   value     = tolist(azuread_application.example.password).0.value
 }
+
+
+# Create Service Principal
+resource "azuread_service_principal" "example_sp" {
+  client_id                    = azuread_application.example.client_id # Fixed reference
+  app_role_assignment_required = true
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
+# Assign API Permissions to Service Principal
+resource "azuread_app_role_assignment" "msgraph_roles" {
+  for_each            = toset(["User.Read.All", "Group.Read.All"])
+  app_role_id         = data.azuread_service_principal.msgraph.app_role_ids[each.value]
+  principal_object_id = azuread_service_principal.example_sp.object_id
+  resource_object_id  = data.azuread_service_principal.msgraph.object_id
+}
+
+# Get Azure AD Domains
+data "azuread_domains" "aad_domains" {}
+
+# Create Users
+resource "azuread_user" "example" {
+  for_each            = toset(var.users)
+  user_principal_name = "${each.value}@${data.azuread_domains.aad_domains.domains.0.domain_name}"
+  display_name        = each.value
+  password            = "Password1234!"
+}
+
+# Get Current Client Configuration
+data "azuread_client_config" "current" {}
+
+# Create Groups
+resource "azuread_group" "groups" {
+  for_each = toset(var.groups)
+
+  display_name     = each.value
+  owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
+}
+
+# Assign Users to Groups (2 users per group)
+resource "azuread_group_member" "group_members" {
+  for_each = { for idx, user in var.users : idx => user if idx < length(var.groups) * 2 }
+
+  group_object_id  = azuread_group.groups[element(var.groups, floor(each.key / 2))].id
+  member_object_id = azuread_user.example[each.value].id
+}
